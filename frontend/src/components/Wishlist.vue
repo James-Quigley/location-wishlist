ecdb44ab1f3962c5e0301c23ce3560b87f3037d3
James Quigley <james@quigley.us>
2019-11-17 11:37:57 -0500

    WIP

--- frontend/src/components/Wishlist.vue
+++ frontend/src/components/Wishlist.vue
@@ -3,53 +3,79 @@
     <h1>{{ $route.params.address }}</h1>
 
     <div class="mt-3 mb-5">
-
       <p>Looks like this storefront is empty!</p>
       <p>Submit &amp; vote on what you want this location to be!</p>
 
       <div class="container">
         <div class="row justify-content-center">
           <div class="input-group col-lg-6 col-md-8">
-            <input type="text" class="form-control" placeholder="Bakery? Pizzeria? Bookstore?" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Bakery? Pizzeria? Bookstore?"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            />
             <div class="input-group-append">
-              <button class="btn btn-outline-success" type="button" id="button-addon2">Make your wish</button>
+              <button
+                class="btn btn-outline-success"
+                type="button"
+                id="button-addon2"
+              >Make your wish</button>
             </div>
           </div>
         </div>
       </div>
-
     </div>
 
     <div class="wish-container">
-      <div v-for="wish in wishes" v-bind:key="wish.id" class="card wish mr-3 mb-3">
+      <div v-for="wish in location.ideas" v-bind:key="wish.idea" class="card wish mr-3 mb-3">
         <div class="card-body">
-          <h5 class="card-title">{{ wish }}</h5>
-          <p class="card-text">9 votes</p>
+          <h5 class="card-title">{{ wish.idea }}</h5>
+          <p class="card-text">{{ wish.votes }}</p>
           <a href="#" class="btn btn-outline-success">-1</a>
           <a href="#" class="btn btn-outline-success">+1</a>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
+import gql from "graphql-tag";
+
+const getLocation = gql`
+  query GetLocation($address: String!){
+    location(address: $address) {
+      address
+      suggestions{
+        idea
+        votes
+      }
+    }
+  }`;
+
 export default {
-  name: 'Wishlist',
+  name: "Wishlist",
   data: () => ({
-    wishes: [
-      'Barbecue', 'Beer hall', 'Archery range',
-      'Barbecue', 'Beer hall', 'Archery range',
-      'Barbecue', 'Beer hall', 'Archery range',
-    ]
+    location: {
+      address: "",
+      suggestions: []
+    }
   }),
+  apollo: {
+    location: {
+      query: getLocation(this.$route.params.address)
+    }
+  },
   props: { address: String }
-}
+};
 </script>
 
 <style scoped>
-h3 { margin: 40px 0 0; }
+h3 {
+  margin: 40px 0 0;
+}
 
 .card-body a {
   margin: 0 10px;
@@ -64,6 +90,7 @@ h3 { margin: 40px 0 0; }
   justify-content: center;
 }
 
-.wish { margin-top: 5px; }
-
+.wish {
+  margin-top: 5px;
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,9 +5,37 @@ import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
+
+// Create an http link:
+
+const link = new HttpLink({
+  uri:
+    process.env.NODE_ENV == "production"
+      ? "https://vacant.astoria.app/graphql"
+      : "http://localhost:3000/graphql",
+  fetch
+});
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache({
+    addTypename: true
+  })
+});
+
 Vue.config.productionTip = false;
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: client
+});
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
